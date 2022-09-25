1dc4bd1fc065d2341ea14d9c6acee6234faf6c97
voidsouvik <112721951+voidsouvik@users.noreply.github.com>
2022-09-25 23:21:50 +0530

    Add files via upload

---
+++ logging.js
@@ -0,0 +1,21 @@
+ const ethers = require("ethers");
+ const  abi = require("../abis/abi.json");
+ require("dotenv").congif();
+ 
+ async function main()
+ {
+    const prjAddress= "0xC34A11cF9399c5eAC1448f519578427ac0b55C3E";
+    const provider = new ethers.providers.WebSocketProvider('wss://eth-mainnet.g.alchemy.com/v2/gMFM21aT5iacac8b7DTQcyq_E3IRFexO{process.env.ALCHEMY_WEBSOCKET}');
+ const contract = new ethers.Contract(prjAddress,abi,provider);
+ contract.on("Transfer",(from, to, value, event)=>{
+    let info ={
+        from: from,
+        to: to,
+        value:ethers.utils.formatUnits(value,6),
+        data: event,
+    };
+    console.log(JSON.stringify(info,null,4));
+
+ });
+}
+
